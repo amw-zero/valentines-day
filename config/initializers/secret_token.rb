437a577541970eaefd04e4275a495e925bdd1603
Alex Weisberger <alex.m.weisberger@gmail.com>
2014-02-13 14:37:22 -0500

    Initial commit: rails valentines day app :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ValentinesDay::Application.config.secret_key_base = '3ded2b28cf6967f6ea8e4bfa8b075de225e878caabaa3b52789fd38cd7233e1e135dadc7c1b59ede122be55525f6e7fafec637e55cb968941e3fce88ebbbec61'
